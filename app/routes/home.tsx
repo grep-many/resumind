61dc9f91950ea6c56137f521e5fe93db7eea712c
Manish <manishkumar9540898@gmail.com>
2025-11-25 10:09:37 +0530

    chore(init):initialized project with react with vite build and react router v7 with base styles and assets with base styles

---
+++ app/routes/home.tsx
@@ -0,0 +1,12 @@
+import type { Route } from './+types/home';
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: 'Resumind' },
+    { name: 'description', content: 'Smart feedback for your dream job!' },
+  ];
+}
+
+export default function Home() {
+  return <main></main>;
+}
